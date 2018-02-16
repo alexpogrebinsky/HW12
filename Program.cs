9d39e8ca6a86d0d4008ccf38a058631dab7f37ce
Alexander A Pogrebinsky <alex@pogrebinskygroup.com>
2018-02-16 11:46:50 -0500

    hw12 console

--- Program.cs
+++ Program.cs
@@ -4,12 +4,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HW12.Console
+namespace HW12
 {
     class Program
     {
+
+
         static void Main(string[] args)
+        {string UserText, reverseText = "";
+            try
+            {
+                do
+                {
+                    
+                    
+                    Console.WriteLine("Please enter a text to see if it is a palindrome or enter 'Exit' to leave:");
+                    UserText = Console.ReadLine();
+
+                    if (UserText == "Exit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Congrats it is a Palindrome!");
+                        reverseTextCheck();
+                        
+                    }
+
+                }
+                while (true);
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured");
+            }
+
+
+
+
+            Console.ReadLine();
+            Console.ReadKey();
+
+        }
+
+
+
+
+        static void reverseTextCheck()
         {
+
+            string UserText, reverseText = "";
+            int Length;
+        
+            Console.WriteLine("Please enter a text to see if it is a palindrome or enter 'Exit' to leave:");
+             UserText = Console.ReadLine();
+
+            Length = UserText.Length - 1;
+            while (Length >= 0)
+            {
+                reverseText = reverseText + UserText[Length];
+                Length--;
+            }
+
+        Console.WriteLine("Reverse is {0}", reverseText);
+
+            if
+                (reverseText == UserText)
+            {
+                Console.WriteLine("Congrats " + reverseText + " is a Palindrome!"); }
+              
+           
+            else
+            {
+                Console.WriteLine("Sorry it is not a Palindrome!");
+            }
         }
     }
 }
---
+++ reverseTextCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace HW12
+{
+    [Serializable]
+    internal class reverseTextCheck : Exception
+    {
+        public reverseTextCheck()
+        {
+        }
+
+        public reverseTextCheck(string message) : base(message)
+        {
+        }
+
+        public reverseTextCheck(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected reverseTextCheck(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
